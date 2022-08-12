eab1b721c3188195398734a32fc7d63be8fcf587
Facundo <97543231+Facubra25@users.noreply.github.com>
2022-08-11 22:54:04 -0300

    Calculadora

---
+++ src/componentes/Imput.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../hojas-de-estilo/Imput.css';
+
+const Imput = ({ input }) => (
+    <div className="input">
+        {input}
+    </div>
+);
+
+export default Imput;
